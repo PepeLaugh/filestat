5519e62f5e8ab247814741c1a10aed56d6498345
PepeLaugh <46378008+PepeLaugh@users.noreply.github.com>
2019-07-17 14:43:51 +0200

    Add files via upload

--- filestat.c
+++ filestat.c
@@ -16,27 +16,49 @@ char* date(time_t time){
     ctime_no_nl = strtok(ctime(&currentTime), "\n");
     return ctime_no_nl;
 }
-// Cerca se il file è stato richiesto almeno una volta
-/*
-void search (char* pathname) {
-    FILE* fr = fopen("home/navi/workspace/Ulamogv2/filestat.db","r+");
-    char temp[512];
-    int line_num = 1;
-	int find_result = 0;
-    char *buffer = malloc(strlen(pathname)+sizeof("<"));
-    strcpy(buffer,pathname);
-    strcat(buffer, ">");
-	while(fgets(temp, 512, fr) != NULL) {
-		if((strstr(temp, buffer)) != NULL) {
-			printf("A match found on line: %d\n", line_num);
-			printf("\n%s\n", temp);
-		}
-		line_num++;
-	}
-    free(buffer);
-    fclose(fr);
+
+void verbosePrint (char *pathname, struct stat fileStat) {
+    FILE* fout = fopen(pathname,"a+");
+    fprintf(fout,"<%s> ",date(time(NULL)));
+    printf("%s ",date(time(NULL)));
+    fprintf(fout,"<%d> ",fileStat.st_uid);
+    printf("%d ",fileStat.st_uid);
+    fprintf(fout,"<%d> ",fileStat.st_gid);
+    printf("%d ",fileStat.st_gid);
+    fprintf(fout,"<%ld> ",fileStat.st_size);
+    printf("%ld ",fileStat.st_size);
+    fprintf(fout,(S_ISDIR(fileStat.st_mode)) ? "<d" : "<-");
+    printf((S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+    fprintf(fout,(fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    printf((fileStat.st_mode & S_IRUSR) ? "r" : "-");
+    fprintf(fout,(fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    printf((fileStat.st_mode & S_IWUSR) ? "w" : "-");
+    fprintf(fout,(fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    printf((fileStat.st_mode & S_IXUSR) ? "x" : "-");
+    fprintf(fout,(fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    printf((fileStat.st_mode & S_IRGRP) ? "r" : "-");
+    fprintf(fout,(fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    printf((fileStat.st_mode & S_IWGRP) ? "w" : "-");
+    fprintf(fout,(fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    printf((fileStat.st_mode & S_IXGRP) ? "x" : "-");
+    fprintf(fout,(fileStat.st_mode & S_IROTH) ? "r" : "-");
+    printf((fileStat.st_mode & S_IROTH) ? "r" : "-");
+    fprintf(fout,(fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    printf((fileStat.st_mode & S_IWOTH) ? "w" : "-");
+    fprintf(fout,(fileStat.st_mode & S_IXOTH) ? "x> " : "-> ");
+    printf((fileStat.st_mode & S_IXOTH) ? "x " : "- ");
+    fprintf(fout,"<%s> ",date(fileStat.st_atime));
+    printf("%s ",date(fileStat.st_atime));
+    fprintf(fout,"<%s> ",date(fileStat.st_mtime));
+    printf("%s ",date(fileStat.st_mtime));
+    fprintf(fout,"<%s> ",date(fileStat.st_ctime));
+    printf("%s ",date(fileStat.st_ctime));
+    fprintf(fout,"<%ld> \n",fileStat.st_nlink);
+    printf("%ld\n\n",fileStat.st_nlink);
+    fprintf(fout,"###\n");
+    fclose(fout);
 }
-*/
+
 void printStat (char *pathname, struct stat fileStat) {
     FILE* fout = fopen(pathname,"a+");
     fprintf(fout,"<%s> ",date(time(NULL)));
@@ -52,15 +74,16 @@ void printStat (char *pathname, struct stat fileStat) {
     fprintf(fout,(fileStat.st_mode & S_IXGRP) ? "x" : "-");
     fprintf(fout,(fileStat.st_mode & S_IROTH) ? "r" : "-");
     fprintf(fout,(fileStat.st_mode & S_IWOTH) ? "w" : "-");
-    fprintf(fout,(fileStat.st_mode & S_IXOTH) ? "x>" : "->");
-    fprintf(fout," <%s> ",date(fileStat.st_atime));
-    fprintf(fout," <%s> ",date(fileStat.st_mtime));
-    fprintf(fout," <%s> \n",date(fileStat.st_ctime));
+    fprintf(fout,(fileStat.st_mode & S_IXOTH) ? "x> " : "-> ");
+    fprintf(fout,"<%s> ",date(fileStat.st_atime));
+    fprintf(fout,"<%s> ",date(fileStat.st_mtime));
+    fprintf(fout,"<%s> ",date(fileStat.st_ctime));
+    fprintf(fout,"<%ld> \n",fileStat.st_nlink);
     fprintf(fout,"###\n");
     fclose(fout);
 }
 // Metodo per la scrittura del file di output
-void writeOut (char *pathname, bool link) {
+void writeOut (char *pathname, bool link, bool verb) {
     struct stat fileStat;
     if (link) {
         // Stats del link
@@ -69,20 +92,26 @@ void writeOut (char *pathname, bool link) {
         // Stats del file a cui fa riferimento il link
         stat(pathname,&fileStat);
     }
-    FILE* fout = fopen("/home/navi/workspace/Ulamogv2/filestat.db","a+");
-    // Scrittura dei parametri richiesti
+    FILE* fout = fopen("filestat.db","a+");
     char temp[512];
-    FILE* copy = fopen("/home/navi/workspace/Ulamogv2/copy.db","a+");
+    // Creazione di una copia del file di output
+    FILE* copy = fopen("copy.db","a+");
+    // Flag per l'eventuale presenza del file richiesto
 	int find_result = 0;
+    // Modifica del pathname per facilitare la scansione
     char *buffer = malloc(strlen(pathname)+sizeof("<"));
     strcpy(buffer,pathname);
     strcat(buffer, ">");
 	while(fgets(temp, 512, fout) != NULL) {
+        // Ciclo il file di output copiando ogni riga
         fputs(temp,copy);
 		if((strstr(temp, buffer)) != NULL) {
+            // Se trova una stringa uguale al pathname, aggiorno il flag
             find_result = 1;
             while(fgets(temp, 512, fout) != NULL){
+                // Scansiono ogni riga finchè non trovo la fine del file desiderato
                 if (strstr(temp,"###") != NULL) {
+                    // Aggiungo una nuova entry
                     fprintf(copy,"<%s> ",date(time(NULL)));
                     fprintf(copy,"<%d> ",fileStat.st_uid);
                     fprintf(copy,"<%d> ",fileStat.st_gid);
@@ -96,16 +125,19 @@ void writeOut (char *pathname, bool link) {
                     fprintf(copy,(fileStat.st_mode & S_IXGRP) ? "x" : "-");
                     fprintf(copy,(fileStat.st_mode & S_IROTH) ? "r" : "-");
                     fprintf(copy,(fileStat.st_mode & S_IWOTH) ? "w" : "-");
-                    fprintf(copy,(fileStat.st_mode & S_IXOTH) ? "x>" : "->");
-                    fprintf(copy," <%s> ",date(fileStat.st_atime));
-                    fprintf(copy," <%s> ",date(fileStat.st_mtime));
-                    fprintf(copy," <%s> \n",date(fileStat.st_ctime));
+                    fprintf(copy,(fileStat.st_mode & S_IXOTH) ? "x> " : "-> ");
+                    fprintf(copy,"<%s> ",date(fileStat.st_atime));
+                    fprintf(copy,"<%s> ",date(fileStat.st_mtime));
+                    fprintf(copy,"<%s> ",date(fileStat.st_ctime));
+                    fprintf(copy,"<%ld> \n",fileStat.st_nlink);
                     fprintf(copy,"###\n");
+                    // Copio il resto del file
                     while(fgets(temp, 512, fout) != NULL) {
                         fputs(temp,copy);
                     }
                     break;
                 }
+                // Copio finchè non trovo la stringa desiderata
                 fputs(temp,copy);
             }
 		}
@@ -114,28 +146,36 @@ void writeOut (char *pathname, bool link) {
         }
 	}
     free(buffer);
+    fclose(copy);
     if (find_result == 0){
+        // Se non è stato trovato un match, aggiungo il pathname del nuovo file e scansiono
         fprintf(fout,"# <%s>\n",pathname);
-        fclose(fout);;
-        freopen("/home/navi/workspace/Ulamogv2/copy.db","w",copy);
-        fclose(copy);
-        printStat("/home/navi/workspace/Ulamogv2/filestat.db",fileStat);
-    } else {
+        if (verb) {
+            printf("%s \n",pathname);
+        }
         fclose(fout);
-        fclose(copy);
-        remove("/home/navi/workspace/Ulamogv2/filestat.db");
-        rename("/home/navi/workspace/Ulamogv2/copy.db","/home/navi/workspace/Ulamogv2/filestat.db");
+        remove("copy.db");
+        if (verb) {
+            verbosePrint("filestat.db",fileStat);
+        } else {
+            printStat("filestat.db",fileStat);
+        }
+
+    } else {
+        // Se è stato trovato un match, scambio la copia aggiornata con il vecchio file
+        fclose(fout);
+        remove("filestat.db");
+        rename("copy.db","filestat.db");
     }
 }
 
 // Metodo che cerca ricorsivamente i file all'interno delle cartelle
-int recWrite (char *pathname, bool link) {
+int recWrite (char *pathname, bool link, bool verb) {
     DIR *dir;
     struct dirent *ent;
-    writeOut(pathname,link);
+    writeOut(pathname,link,verb);
     // Apre e scansiona la cartella
     if ((dir = opendir (pathname)) != NULL) {
-        /* print all the files and directories within directory */
         while ((ent = readdir (dir)) != NULL) {
             // Salta un ciclo se il nome è . o ..
             if (strcmp(".",ent->d_name) == 0 || strcmp("..",ent->d_name) == 0) {
@@ -148,11 +188,11 @@ int recWrite (char *pathname, bool link) {
             strcat(buffer,ent->d_name);
             // Se trova una cartella, chiama il metodo ricorsivamente
             if (ent->d_type == 4) {
-                recWrite(buffer,link);
+                recWrite(buffer,link,verb);
                 free(buffer);     
             } else {
                 // Altrimenti scrive nel file di output e continua con la scansione
-                writeOut(buffer,link);
+                writeOut(buffer,link,verb);
                 free(buffer);
             }
         }
@@ -163,6 +203,11 @@ int recWrite (char *pathname, bool link) {
     }
 }
 
-int main (void){
-    recWrite("/home/navi/Documents/test",false);
+void noscan(char *pathname){
+    char temp[512];
+    FILE* f = fopen(pathname,"r");
+    while (fgets(temp,512,f) != NULL) {
+        printf("%s", temp);
+    }
+    fclose(f);
 }
